62525ca4a3fa4862dba98cda6f95c62882e8f85c
Isaac Chung <chungisaac1010@gmail.com>
2023-07-02 11:01:54 -0700

    Created a template website localhost link.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/*
+* Removed unnecessary imports and App structures to make code look cleaner.
+*/
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const  App = () => {
   return (
     <div>
       Personal (Portfolio) Website
