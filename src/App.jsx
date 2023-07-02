1cf3ee6fdbf417b3f142f3959ffa99c801b20686
Isaac Chung <chungisaac1010@gmail.com>
2023-07-02 11:55:33 -0700

    Made modifications to website and added a logo for myself.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 /*
+* This is the skeleton (structural component) of my website.
 * Removed unnecessary imports and App structures to make code look cleaner.
 */
 
+import { BrowserRouter } from "react-router-dom"; 
+import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas} from './components'; 
+
 const  App = () => {
   return (
-    <div>
-      Personal (Portfolio) Website
-    </div>
+    <BrowserRouter>
+      <div className = 'relative z-0 bg-primary'>
+        
+        <div className = 'bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+           <Navbar />
+           <Hero />
+        </div>
+
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+
+        <div className = 'relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+
+      </div>
+    </BrowserRouter>
   )
 }
 
