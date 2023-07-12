df288c843cce3cbcad5bf7f1922b830fb7560485
Isaac Chung <chungisaac1010@gmail.com>
2023-07-12 08:59:41 -0700

    Changes and adding dist.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ijoonc-portfolio',
+  base: '/ijoonc-portfolio/',
   plugins: [react()]
 })
