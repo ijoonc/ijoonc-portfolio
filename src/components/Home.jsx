09330916efa59e551929f751041c0aefe45381c5
Isaac Chung <chungisaac1010@gmail.com>
2023-07-24 21:52:04 -0700

    Added a scrolling animation for the home section.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { SectionWrapper } from '../hoc'
 
 const Home = () => {
   return (
@@ -6,4 +7,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default SectionWrapper(Home, "home")
