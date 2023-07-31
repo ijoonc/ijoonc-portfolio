55f757a1bba9face595b2fb82e430082ffac378a
Isaac Chung <chungisaac1010@gmail.com>
2023-07-30 22:13:52 -0700

    Started on Work Experience and solved Home button and scrolling issue.

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,14 @@ const  App = () => {
   return (
     <BrowserRouter>
       <div className = 'relative z-0 bg-primary'>
+
+        <Home />
         
         <div className = 'bg-hero-pattern bg-cover bg-no-repeat bg-center'>
            <Navbar />
            <Hero />
         </div>
         
-        <Home />
         <About />
         <Experience />
         <Tech />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import { SectionWrapper } from '../hoc'
 
 const Home = () => {
+
+  const handleHomeClick = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0; 
+  }
+
   return (
-    <div></div>
+    <div options = {{speed: 450}}>
+      <a href = '#' className = 'hash-span'
+         onClick = {handleHomeClick}
+         id = "home">
+      </a>
+    </div>
   )
 }
 
-export default SectionWrapper(Home, "home")
+export default Home
